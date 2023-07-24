6d5fc8c622e22ec57b40d5c6c96dc07003232dc3
Akila Sachintha <akilasachintha@gmail.com>
2023-07-24 11:58:06 +0530

    Initial Commit - Project Setup

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import TopNavigationBar from '@/components/topNavigationBar/TopNavigationBar';
+
+export default function Home() {
+  return (
+    <main>
+      <TopNavigationBar/>
+      <div className='flex justify-center'>
+        <h1>Hello Icode Web</h1>
+      </div>
+    </main>
+  )
+}
